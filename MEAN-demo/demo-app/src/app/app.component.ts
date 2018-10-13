1c61998875315c3db6c351bed4b314fff8a2f14d
ramasubbaiya <ramasubbaiya@gmail.com>
2018-10-13 14:35:36 -0400

    adding observables from rxjs

--- MEAN-demo/demo-app/src/app/app.component.ts
+++ MEAN-demo/demo-app/src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Post } from './posts/post.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  storedPosts: Post[] = [];
 
-  onPostAdded(post) {
-    this.storedPosts.push(post);
-  }
+export class AppComponent {
 }
--- MEAN-demo/demo-app/src/app/posts/post-list/post-list.component.ts
+++ MEAN-demo/demo-app/src/app/posts/post-list/post-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+
 
 @Component({
   selector: 'app-post-list',
@@ -9,14 +11,28 @@ import { PostsService } from '../posts.service';
   styleUrls: ['./post-list.component.scss']
 })
 
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   { title: 'First Post', content: 'This is the first post'},
   //   { title: 'Second Post', content: 'This is the second post'},
   //   { title: 'Third Post', content: 'This is the third post'},
   // ];
 
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
-   constructor(public postsService: PostsService) {}
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    // this will remove subscription and remove memory leak
+    this.postsSub.unsubscribe();
+  }
 }
