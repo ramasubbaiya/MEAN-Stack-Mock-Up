d30df84cd8603326f44134921f13f8555a7ef7f5
ramasubbaiya <ramasubbaiya@gmail.com>
2018-10-14 12:39:42 -0400

    adding posts service and injectables

--- MEAN-demo/demo-app/src/app/posts/post-list/post-list.component.ts
+++ MEAN-demo/demo-app/src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -16,4 +17,6 @@ export class PostListComponent {
   // ];
 
   @Input() posts: Post[] = [];
+
+   constructor(public postsService: PostsService) {}
 }
