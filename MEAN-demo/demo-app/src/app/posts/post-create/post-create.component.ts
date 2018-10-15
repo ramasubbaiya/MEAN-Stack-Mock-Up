94c0addf5d4f227c47b73a70259008f3a9131578
ramasubbaiya <ramasubbaiya@gmail.com>
2018-10-15 12:09:59 -0400

    adding forms and validation

--- MEAN-demo/demo-app/src/app/posts/post-create/post-create.component.ts
+++ MEAN-demo/demo-app/src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Post } from '../post.model';
+
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -12,10 +14,13 @@ export class PostCreateComponent {
   enteredContent = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
