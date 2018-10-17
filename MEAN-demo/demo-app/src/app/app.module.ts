19bcda04679b35855bb3bb5e13b77781ae45bc28
ramasubbaiya <ramasubbaiya@gmail.com>
2018-10-17 18:48:13 -0400

    added angular material design and header component

--- MEAN-demo/demo-app/src/app/app.module.ts
+++ MEAN-demo/demo-app/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { PosCreateComponent } from './posts/post-create/post-create.component';
 
-@NgModule({
+
+ @NgModule({
   declarations: [
     AppComponent,
-    PosCreateComponent
+    PosCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
